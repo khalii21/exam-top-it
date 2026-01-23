45b18f6c8b21680a60c98ceeeaebc339d2fc2d3d
Халиков Мират Русланович <miratkhalikov007@gmail.com>
2026-01-23 13:37:13 +0300

    SumNegative is added

--- khalikov.mirat/W1/main.cpp
+++ khalikov.mirat/W1/main.cpp
@@ -3,7 +3,7 @@
 struct ITrait
 {
   virtual void  operator()(int a) = 0;
-  virtual size_t operator()() = 0;
+  virtual int operator()() = 0;
   virtual ~ITrait() = default;
 };
 
@@ -17,21 +17,69 @@ struct Size : ITrait
   {
     k_++;
   }
-  size_t operator()() override
+  int operator()() override
   {
     return k_;
   }
   private:
-    size_t k_;
+    int k_;
+};
+
+struct SumPositive : ITrait
+{
+  SumPositive()
+ {
+    k_ = 0;
+  }
+  void operator()(int a) override
+  {
+    if (a > 0)
+    {
+      k_ += a;
+    }
+  }
+  int operator()() override
+  {
+    return k_;
+  }
+  private:
+    int k_;
+};
+
+struct SumNegative : ITrait
+{
+  SumNegative()
+  {
+    k_ = 0;
+  }
+  void operator()(int a) override
+  {
+    if (a < 0)
+    {
+      k_ += a;
+    }
+  }
+  int operator()() override
+  {
+    return k_;
+  }
+  private:
+    int k_;
 };
 
 int main()
 {
   int a = 0;
   Size size;
+  SumPositive sump;
+  SumNegative sumn;
   while (std::cin >> a)
   {
     size(a);
+    sump(a);
+    sumn(a);
   }
   std::cout << "SIZE = " << size() << "\n";
+  std::cout << "SUMP = " << sump() << "\n";
+  std::cout << "SUMN = " << sumn() << "\n";
 }
